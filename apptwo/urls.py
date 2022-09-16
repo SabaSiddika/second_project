dd1e07bf39fa4ac84dfb41a04565e44aab5dc719
Saba Siddika <60462029+SabaSiddika@users.noreply.github.com>
2022-09-16 12:12:26 +0600

    Add files via upload

---
+++ apptwo/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from apptwo import views
+urlpatterns = [
+    path('',views.users,name ='users'),
+]
---
+++ apptwo/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+#from apptwo.models import User
+from apptwo.forms import NewUserForm
+
+# Create your views here.
+def index(request):
+    return render(request,'apptwo/index.html')
+
+
+def users(request):
+    #user_list = User.objects.order_by('first_name')
+    #user_dict ={'users':user_list}
+   # return render(request,'apptwo/users.html',context=user_dict)
+
+    form = NewUserForm()
+
+    if request.method =="POST":
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+           form.save(commit=True)
+           return index(request)
+
+        else:
+            print("Error form invalid")
+
+    return render(request,'apptwo/users.html',{'form':form})
